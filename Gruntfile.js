4d8de4729256b93f757d2c050bb017254eaf44b1
Long Ouyang <longouyang@gmail.com>
2016-12-08 14:01:40 -0800

    Decruft Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,83 +5,14 @@ var open = require('open');
 var child_process = require('child_process');
 var fs = require('fs');
 
-var jslintSettings = {
-  options: {
-    flags: ['--flagfile .gjslintrc'],
-    reporter: {
-      name: 'console'
-    },
-    force: false
-  },
-  lib: {
-    src: [
-      'Gruntfile.js',
-      'src/*.js',
-    ]
-  }
-};
 module.exports = function(grunt) {
   grunt.initConfig({
-   nodeunit: {
-      all: ['tests/test-*.js']
-    },
-    jshint: {
-      files: [
-        'Gruntfile.js',
-        'src/*.js'
-      ],
-      options: {
-        maxerr: 500,
-        camelcase: true,
-        nonew: true,
-        curly: true,
-        noarg: true,
-        trailing: true,
-        forin: true,
-        noempty: true,
-        node: true,
-        eqeqeq: true,
-        strict: false,
-        evil: true,
-        undef: true,
-        bitwise: true,
-        browser: true,
-        gcl: true,
-        newcap: false
-      }
-    },
-    gjslint: jslintSettings,
-    fixjsstyle: jslintSettings,
-    clean: ['bundle/*.js'],
-    watch: {
-      ad: {
-        files: ['src/*.js'],
-        tasks: ['build']
-      }
-    }
+    clean: ['bundle/*.js']
   });
 
-  function browserifyArgs(args) {
-    // var pkgArg = '';
-    // var requires = _.chain(_.toArray(args))
-    //     .map(function(name) { return ['--require', name]; })
-    //     .flatten().value();
-    // pkgArg = ' -t [' + ['./src/bundle.js'].concat(requires).join(' ') + ']';
-    return ' -t [babelify --presets [react] ] src/index.js -o bundle/webppl-viz.js';
-  }
-
-  grunt.loadNpmTasks('grunt-subgrunt');
-  grunt.loadNpmTasks('grunt-gjslint');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['gjslint', 'nodeunit']);
-  grunt.registerTask('test', ['nodeunit']);
-  grunt.registerTask('lint', ['gjslint']);
-  grunt.registerTask('hint', ['jshint']);
-  grunt.registerTask('fixstyle', ['fixjsstyle']);
+  grunt.registerTask('default', ['bundle']);
 
   grunt.registerTask('bundle', 'Create browser bundle (= css + browserify + uglify)', function() {
     var taskArgs = (arguments.length > 0) ? ':' + _.toArray(arguments).join(':') : '';
@@ -93,6 +24,10 @@ module.exports = function(grunt) {
     var cssSource = fs.readFileSync('src/style.css','utf8');
     fs.writeFileSync('bundle/webppl-viz.css', cssSource)
   })
+
+  function browserifyArgs(args) {
+    return ' -t [babelify --presets [react] ] src/index.js -o bundle/webppl-viz.js';
+  }
 
   grunt.registerTask('browserify', 'Generate "bundle/webppl-viz.js".', function() {
     child_process.execSync('mkdir -p bundle');
