2033560a713350024448f41a1116c82067c70af3
Long Ouyang <longouyang@gmail.com>
2016-12-08 14:11:19 -0800

    Grunt: switch from bundle/ to docs/, remove uglify from default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,36 +7,40 @@ var fs = require('fs');
 
 module.exports = function(grunt) {
   grunt.initConfig({
-    clean: ['bundle/*.js']
+    clean: ['docs/webppl-viz.*'],
+    watch: {
+      scripts: {
+        files: ['src/style.css'],
+        tasks: ['css']
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('default', ['bundle']);
 
-  grunt.registerTask('bundle', 'Create browser bundle (= css + browserify + uglify)', function() {
+  grunt.registerTask('bundle', 'Create browser bundle (= css + browserify)', function() {
     var taskArgs = (arguments.length > 0) ? ':' + _.toArray(arguments).join(':') : '';
-    grunt.task.run('browserify' + taskArgs, 'uglify','css');
+    grunt.task.run('css', 'browserify' + taskArgs);
   });
 
   grunt.registerTask('css', 'Concatenate css files', function() {
-    child_process.execSync('mkdir -p bundle');
     var cssSource = fs.readFileSync('src/style.css','utf8');
-    fs.writeFileSync('bundle/webppl-viz.css', cssSource)
+    fs.writeFileSync('docs/webppl-viz.css', cssSource)
   })
 
   function browserifyArgs(args) {
-    return ' -t [babelify --presets [react] ] src/index.js -o bundle/webppl-viz.js';
+    return ' -t [babelify --presets [react] ] src/index.js -o docs/webppl-viz.js';
   }
 
-  grunt.registerTask('browserify', 'Generate "bundle/webppl-viz.js".', function() {
-    child_process.execSync('mkdir -p bundle');
+  grunt.registerTask('browserify', 'Generate "docs/webppl-viz.js".', function() {
     child_process.execSync('browserify' + browserifyArgs(arguments));
   });
 
   grunt.registerTask('browserify-watch', 'Run the browserify task on fs changes.', function() {
     var done = this.async();
-    child_process.execSync('mkdir -p bundle');
+    child_process.execSync('mkdir -p docs');
     var args = '-v' + browserifyArgs(arguments);
     var p = child_process.spawn('watchify', args.split(' '));
     p.stdout.on('data', grunt.log.writeln);
@@ -44,9 +48,8 @@ module.exports = function(grunt) {
     p.on('close', done);
   });
 
-  grunt.registerTask('uglify', 'Generate "bundle/webppl-viz.min.js".', function() {
-    child_process.execSync('mkdir -p bundle');
-    child_process.execSync('uglifyjs bundle/webppl-viz.js -b ascii_only=true,beautify=false > bundle/webppl-viz.min.js');
+  grunt.registerTask('uglify', 'Generate "docs/webppl-viz.min.js".', function() {
+    child_process.execSync('uglifyjs docs/webppl-viz.js -b ascii_only=true,beautify=false > docs/webppl-viz.min.js');
   });
 
 };
