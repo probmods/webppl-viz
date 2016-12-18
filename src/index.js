a346c93cfdb12928c3f6e829940cab2dc62c49ac
Dor Arad <dorarad@stanford.edu>
2016-12-17 16:30:58 -0800

    remove table printing change (split to a follow-up PR)

--- src/index.js
+++ src/index.js
@@ -1528,11 +1528,11 @@ function lineWrapper() {
 // TODO: support a data frame structure as input
 // and smart hiding if there are too many rows
 function table(obj, options) {
-  //wpEditor is not present if not run in the browser
-  // if (typeof(wpEditor) === 'undefined') {
-  //   console.log("viz.print: no wpEditor, not drawing");
-  //   return;
-  // }
+  wpEditor is not present if not run in the browser
+  if (typeof(wpEditor) === 'undefined') {
+    console.log("viz.print: no wpEditor, not drawing");
+    return;
+  }
 
   options = _.defaults(options || {},
                        {log: false,
@@ -1596,14 +1596,8 @@ function table(obj, options) {
     tableString += rowString;
   })
 
-  if (options.fileName) {
-    require('fs').writeFileSync(options.fileName, tableString);
-    console.log("Rendered to " + options.fileName);
-  }
-  if (!(typeof(wpEditor) === 'undefined')) {
-    var resultContainer = wpEditor.makeResultContainer();
-    resultContainer.innerHTML = tableString;
-  }
+  var resultContainer = wpEditor.makeResultContainer();
+  resultContainer.innerHTML = tableString;
 }
 
 // TODO: display in a wrapped row
